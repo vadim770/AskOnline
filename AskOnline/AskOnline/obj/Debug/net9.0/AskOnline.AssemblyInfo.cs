d54af19be99cd436fa476c9ef3535b4f196a876f
naxash <vadimetlin@gmail.com>
2025-11-10 15:40:50 +0200

    update

--- AskOnline/AskOnline/obj/Debug/net9.0/AskOnline.AssemblyInfo.cs
+++ AskOnline/AskOnline/obj/Debug/net9.0/AskOnline.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AskOnline")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cf4056fa9c6f01ba150f0762a13f4cdf5e15c47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+540a5832773d55d0dd2fd616b1693d2109354147")]
 [assembly: System.Reflection.AssemblyProductAttribute("AskOnline")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AskOnline")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
